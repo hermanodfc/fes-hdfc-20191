711185c238a478e95d59967ded7bdbe82537cdbc
Hermano Costa <hermanodfc@gmail.com>
2019-03-15 23:22:52 -0300

    Inicial.

---
+++ TeleBot.py
@@ -0,0 +1,17 @@
+from telegram.ext import Updater
+updater = Updater(token='893333193:AAGZYL28eJLQxWF7ricLSlKlq8qMQiN_AK8', use_context=True)
+
+dispatcher = updater.dispatcher
+
+import logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                     level=logging.INFO)
+
+def start(update, context):
+    context.bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
+
+from telegram.ext import CommandHandler
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+
+updater.start_polling()
