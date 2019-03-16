12ed3896f5aec7439f106aa757058636deda8307
Hermano Costa <hermanodfc@gmail.com>
2019-03-15 23:39:15 -0300

    Inicial.

--- TeleBot.py
+++ TeleBot.py
@@ -14,4 +14,27 @@ from telegram.ext import CommandHandler
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
 
+
+def echo(update, context):
+    context.bot.send_message(chat_id=update.message.chat_id, text="What?")
+
+from telegram.ext import MessageHandler, Filters
+echo_handler = MessageHandler(Filters.text, echo)
+dispatcher.add_handler(echo_handler)
+
+def caps(update, context):
+    text_caps = ' '.join(context.args).upper()
+    context.bot.send_message(chat_id=update.message.chat_id, text=text_caps)
+
+caps_handler = CommandHandler('caps', caps)
+dispatcher.add_handler(caps_handler)
+
+
+def unknown(update, context):
+    context.bot.send_message(chat_id=update.message.chat_id, text="Sorry, I didn't understand that command.")
+
+unknown_handler = MessageHandler(Filters.command, unknown)
+dispatcher.add_handler(unknown_handler)
+
+
 updater.start_polling()
