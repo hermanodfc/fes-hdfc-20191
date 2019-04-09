694e5aa1c85b43fe39191e015c389d638fcf8cc0
Hermano Costa <hermanodfc@gmail.com>
2019-04-09 12:00:13 -0300

    Código agrupado por funcionalidade. Iniciado o tratamento de mensagens de texto e conversa rudimentar.

--- TeleBot.py
+++ TeleBot.py
@@ -1,40 +1,43 @@
+import Config
+from telegram.ext import CommandHandler
 from telegram.ext import Updater
-updater = Updater(token='893333193:AAGZYL28eJLQxWF7ricLSlKlq8qMQiN_AK8', use_context=True)
+from telegram.ext import MessageHandler, Filters
+import logging
+
+updater = Updater(Config.token, use_context=True)
 
 dispatcher = updater.dispatcher
 
-import logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                      level=logging.INFO)
 
 def start(update, context):
-    context.bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
-
-from telegram.ext import CommandHandler
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
-
+    context.bot.send_message(chat_id=update.message.chat_id, text="Olá! Eu sou um robô. Posso ajudá-lo?")
 
 def echo(update, context):
-    context.bot.send_message(chat_id=update.message.chat_id, text="What?")
-
-from telegram.ext import MessageHandler, Filters
-echo_handler = MessageHandler(Filters.text, echo)
-dispatcher.add_handler(echo_handler)
+    text = update['message']['text']
+    if text.lower() == "oi":
+        context.bot.send_message(chat_id=update.message.chat_id, text="Posso ajudá-lo?")
+    else:
+        context.bot.send_message(chat_id=update.message.chat_id, text="Desculpe, não entendi. Meu vocabulário ainda é muito restrito.")
 
 def caps(update, context):
     text_caps = ' '.join(context.args).upper()
     context.bot.send_message(chat_id=update.message.chat_id, text=text_caps)
 
-caps_handler = CommandHandler('caps', caps)
-dispatcher.add_handler(caps_handler)
-
-
 def unknown(update, context):
     context.bot.send_message(chat_id=update.message.chat_id, text="Sorry, I didn't understand that command.")
 
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+
+caps_handler = CommandHandler('caps', caps)
+dispatcher.add_handler(caps_handler)
+
+echo_handler = MessageHandler(Filters.text, echo)
+dispatcher.add_handler(echo_handler)
+
 unknown_handler = MessageHandler(Filters.command, unknown)
 dispatcher.add_handler(unknown_handler)
-
 
 updater.start_polling()
